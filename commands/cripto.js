8f1fba7099ad3b37e54ec0a00f03f00d81b7ac68
João Pedro <jpfdc9@gmail.com>
2022-02-04 00:34:50 -0300

    adding currency parameter and change everything to english

--- commands/cripto.js
+++ commands/cripto.js
@@ -5,19 +5,25 @@ const service = require('../services/CoinGeckoService');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('cripto')
-		.setDescription('retorna valor da cripto moeda informada')
+		.setDescription('returns the crypto coin value')
 		.addStringOption((option) =>
-			option.setName('coin').setDescription('teste').setRequired(true)
+			option.setName('coin').setDescription('crypto coin').setRequired(true)
+		)
+		.addStringOption((option) =>
+		option.setName('currencies').setDescription('In real life currency CSV style exp: usd,brl').setRequired(true)
 		),
 
 	async execute(interaction) {
-		// await interaction.({
-		// 	content: interaction.options.getStringOption('coin'),
-		// });
 		const coin = interaction.options.getString('coin');
-		const data = await service.getCoin(coin);
+		const currencies = interaction.options.getString('currencies');
+	
+		const data = await service.getCoin({coin, currencies});
+		let returnString = `Coin: ${coin}\n`
+		for(const [key, value] of Object.entries(data)){
+			returnString += `${key}: ${value}\n`
+		}
 		await interaction.reply(
-			`moeda: ${coin} \n usd: ${data.usd} \n brl: ${data.brl}`
+			returnString
 		);
 	},
 };
--- services/CoinGeckoService.js
+++ services/CoinGeckoService.js
@@ -3,10 +3,17 @@ const CoinGecko = require('coingecko-api');
 const client = new CoinGecko();
 
 module.exports = {
-	async getCoin(coin = 'bitcoin') {
+	async getCoin({coin, currencies}) {
+		coin = coin.toLowerCase();
+		currencies = currencies.toLowerCase();
+		let currencyArray = [];
+		if ( currencies.includes(',') ){
+			currencyArray = currencies.split(',')
+		}
+
 		const data = await client.simple.price({
 			ids: coin,
-			vs_currencies: ['usd', 'brl'],
+			vs_currencies: currencyArray.length === 0 ? currencies : currencyArray,
 		});
 		return data.data[coin];
 	},
